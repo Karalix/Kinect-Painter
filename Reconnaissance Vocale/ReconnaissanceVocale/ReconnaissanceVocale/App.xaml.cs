4050890da5483e40b8d58fff805b09275e8580b2
Mathieu <mathieu_dufour@hotmail.fr>
2014-02-11 16:16:00 +0100

    Ajout reconnaissance vocale
    3 grammar : base, dessin et objet
    Plusieurs items possibles pour chaque grammar, sachant que on a celui de
    base chargé d'abord.
    Test à faire, ne fonctionne pas encore.

---
+++ Reconnaissance Vocale/ReconnaissanceVocale/ReconnaissanceVocale/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReconnaissanceVocale
+{
+    using System.Windows;
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Reconnaissance Vocale/ReconnaissanceVocale/ReconnaissanceVocale/MainWindow.xaml.cs
@@ -0,0 +1,287 @@
+﻿
+
+namespace ReconnaissanceVocale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Documents;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+    using System.Speech.AudioFormat;
+    using System.Speech.Recognition;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Resource key for medium-gray-colored brush.
+        /// </summary>
+        private const string MediumGreyBrushKey = "MediumGreyBrush";
+
+        /// <summary>
+        /// Active Kinect sensor.
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Speech recognition engine using audio data from Kinect.
+        /// </summary>
+        private SpeechRecognitionEngine speechEngine;
+
+        /// <summary>
+        /// List of all UI span elements used to select recognized text.
+        /// </summary>
+        private List<Span> recognitionSpans;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the metadata for the speech recognizer (acoustic model) most suitable to
+        /// process audio from Kinect device.
+        /// </summary>
+        /// <returns>
+        /// RecognizerInfo if found, <code>null</code> otherwise.
+        /// </returns>
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "fr-FR".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Execute initialization tasks.
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                try
+                {
+                    // Start the sensor!
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    // Some other application is streaming from the same Kinect sensor
+                    this.sensor = null;
+                }
+            }
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null != ri)
+            {
+                recognitionSpans = new List<Span> { DessinSpan, ObjetSpan };
+
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                // Create a grammar from grammar definition XML file.
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.GrammarBase)))
+                {
+                    var g = new Grammar(memoryStream);
+                    speechEngine.LoadGrammar(g);
+                }
+
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        /// <summary>
+        /// Execute uninitialization tasks.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.AudioSource.Stop();
+
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+        }
+
+        /// <summary>
+        /// Remove any highlighting from recognition instructions.
+        /// </summary>
+        private void ClearRecognitionHighlights()
+        {
+            foreach (Span span in recognitionSpans)
+            {
+                span.Foreground = (Brush)this.Resources[MediumGreyBrushKey];
+                span.FontWeight = FontWeights.Normal;
+            }
+
+            BaseBlock.Visibility = System.Windows.Visibility.Visible;
+            DessinBlock.Visibility = System.Windows.Visibility.Hidden;
+            ObjetBlock.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// Handler for recognized speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Speech utterance confidence below which we treat speech as if it hadn't been heard
+            const double ConfidenceThreshold = 0.3;
+
+            ClearRecognitionHighlights();
+
+            if (e.Result.Confidence >= ConfidenceThreshold)
+            {
+                switch (e.Result.Semantics.Value.ToString())
+                {
+                    case "Dessin":
+                        DessinSpan.Foreground = Brushes.Green;
+                        DessinSpan.FontWeight = FontWeights.Bold;
+
+                        //Launch GrammarDessin
+
+                        //changeSpeech(new SpeechRecognitionEngine(GetKinectRecognizer()));
+                        changeSpeech("Dessin");
+
+                        BaseBlock.Visibility = System.Windows.Visibility.Hidden;
+                        DessinBlock.Visibility = System.Windows.Visibility.Visible;
+
+
+                        break;
+
+                    case "Objet":
+                        ObjetSpan.Foreground = Brushes.Green;
+                        DessinSpan.FontWeight = FontWeights.Bold;
+                       
+                        //Launch GrammarObjet
+
+                        //changeSpeech(new SpeechRecognitionEngine(GetKinectRecognizer()));
+                        changeSpeech("Objet");
+
+                        BaseBlock.Visibility = System.Windows.Visibility.Hidden;
+                        ObjetBlock.Visibility = System.Windows.Visibility.Visible;
+
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for rejected speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            ClearRecognitionHighlights();
+        }
+
+        private void changeSpeech(string grammar)
+        {
+            ClearRecognitionHighlights();
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if(grammar == "DESSIN" && null != ri)
+            {
+                recognitionSpans = new List<Span> { BrosseSpan, PinceauSpan, CrayonSpan };
+
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                // Create a grammar from grammar definition XML file.
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.GrammarDessin)))
+                {
+                    var g = new Grammar(memoryStream);
+                    speechEngine.LoadGrammar(g);
+                }
+
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else if (grammar == "OBJET" && null != ri)
+            {
+                recognitionSpans = new List<Span> { SphereSpan, PyramideSpan, CubeSpan, ManuelSpan, AutoSpan, NordSpan, SudSpan, EstSpan, OuestSpan };
+
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                // Create a grammar from grammar definition XML file.
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.GrammarObjet)))
+                {
+                    var g = new Grammar(memoryStream);
+                    speechEngine.LoadGrammar(g);
+                }
+
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+    }
+}
