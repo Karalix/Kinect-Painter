960e00f581be69d20b878e05483b5e4e6a8cb106
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2014-03-01 23:13:09 +0100

    Ajout d'une bibliothèque de classe pour la reconnaissance de posture.

---
+++ ReconnaissancePosture/PostureAbstraite.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Samples.Kinect.ColorBasics
+{
+    abstract class PostureAbstraite : IEquatable<PostureAbstraite>
+    {
+        public readonly String Nom
+        {
+            get;
+            private set;
+        }
+
+        public PostureAbstraite(String nom)
+        {
+            Nom = nom;
+        }
+
+        public override int GetHashCode()
+        {
+            return Nom.GetHashCode();
+        }
+
+
+
+        public abstract Boolean reconnaitre(Skeleton squelette);
+
+        public bool Equals(PostureAbstraite other)
+        {
+            return this.Nom.Equals(other.Nom);
+        }
+
+        public override bool Equals(object right)
+        {
+            //check null
+            if (object.ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, right))
+            {
+                return true;
+            }
+
+            if (this.GetType() != right.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals(right as PostureAbstraite);
+        }
+    }
+}
---
+++ ReconnaissancePosture/PostureChangéeEventArgs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Samples.Kinect.ColorBasics
+{
+    class PostureChangéeEventArgs : EventArgs
+    {
+        public String NomPosture
+        {
+            get;
+            private set;
+        }
+
+        public bool EntréeEnPosture
+        {
+            get;
+            private set;
+        }
+
+        public bool SortieDePosture
+        {
+            get;
+            private set;
+        }
+
+        public int NuméroSquelette
+        {
+            get;
+            set;
+        }
+
+        public PostureChangéeEventArgs(String nomPosture, 
+                                        bool entréeEnPosture,
+                                        bool sortieDePosture,
+                                        int numéroSquelette)
+        {
+            this.NomPosture = nomPosture;
+            this.EntréeEnPosture = entréeEnPosture;
+            this.SortieDePosture = sortieDePosture;
+            this.NuméroSquelette = numéroSquelette;
+        }
+    }
+}
